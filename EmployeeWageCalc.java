07b7d3eb9238834af55b89d9573a382e24926658
Ankit Bhat <ankitbhatpro@gmail.com>
2020-05-11 23:37:57 +0530

    Added Calculation of wages for a month

--- EmployeeWageCalc.java
+++ EmployeeWageCalc.java
@@ -10,22 +10,30 @@ public class EmployeeWageCalc
 		int fullTimeHours = 8;
 		int partTimeHours = 4;
 		int dailyEmpWage = 0;
-		int empCheck = (int)Math.floor(Math.random() * 100) % 3;
-		switch(empCheck)
+		int monthlyWage = 0;
+		for(int i=0;i<20;i++)
 		{
-			case 1:
-				System.out.println("Employee is Present and Full Time");
-				dailyEmpWage =(fullTimeHours*wagePerHour);
-				System.out.println("Daily Employee Wage is :" + dailyEmpWage);
-				break;
-			case 2:
-				System.out.println("Employee is Present and Part Time");
-				dailyEmpWage =(partTimeHours*wagePerHour);
-				System.out.println("Daily Employee Wage is :" + dailyEmpWage);
-				break;
-			case 0:
-				System.out.println("Employee is Absent");
-				break;
+			int empCheck = (int)Math.floor(Math.random() * 100) % 3;
+			switch(empCheck)
+			{
+				case 1:
+					System.out.println("Employee is Present and Full Time");
+					dailyEmpWage =(fullTimeHours*wagePerHour);
+					System.out.println("Daily Employee Wage is :" + dailyEmpWage);
+					break;
+				case 2:
+					System.out.println("Employee is Present and Part Time");
+					dailyEmpWage =(partTimeHours*wagePerHour);
+					System.out.println("Daily Employee Wage is :" + dailyEmpWage);
+					break;
+				case 0:
+					System.out.println("Employee is Absent");
+					dailyEmpWage = (0*wagePerHour);
+					System.out.println("Daily Employee Wage is :" + dailyEmpWage);
+					break;
+			}
+			monthlyWage+=dailyEmpWage;
 		}
+		System.out.println("Monthly wage is :" + monthlyWage);
 	}
 }
