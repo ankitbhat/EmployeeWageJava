1654c0e48d08542dab517f3e0458b34a653cecb3
Ankit Bhat <ankitbhatpro@gmail.com>
2020-05-11 23:03:01 +0530

    Solving using switch case

--- EmployeeWageCalc.java
+++ EmployeeWageCalc.java
@@ -9,21 +9,23 @@ public class EmployeeWageCalc
 		int wagePerHour = 20;
 		int fullTimeHours = 8;
 		int partTimeHours = 4;
-		double dailyEmpWage = 0.0;
-		double empCheck = Math.floor(Math.random() * 100) % 3;
-		if(empCheck == FULLTIME)
+		int dailyEmpWage = 0;
+		int empCheck = (int)Math.floor(Math.random() * 100) % 3;
+		switch(empCheck)
 		{
-			System.out.println("Employee is Present and Full Time");
-			dailyEmpWage =(fullTimeHours*wagePerHour);
-			System.out.println("Daily Employee Wage is :" + dailyEmpWage);
+			case 1:
+				System.out.println("Employee is Present and Full Time");
+				dailyEmpWage =(fullTimeHours*wagePerHour);
+				System.out.println("Daily Employee Wage is :" + dailyEmpWage);
+				break;
+			case 2:
+				System.out.println("Employee is Present and Part Time");
+				dailyEmpWage =(partTimeHours*wagePerHour);
+				System.out.println("Daily Employee Wage is :" + dailyEmpWage);
+				break;
+			case 0:
+				System.out.println("Employee is Absent");
+				break;
 		}
-		else if(empCheck == PARTTIME)
-		{
-			System.out.println("Employee is Present and Part Time");
-			dailyEmpWage =(partTimeHours*wagePerHour);
-			System.out.println("Daily Employee Wage is :" + dailyEmpWage);
-		}
-		else
-			System.out.println("Employee is Absent");
 	}
 }
