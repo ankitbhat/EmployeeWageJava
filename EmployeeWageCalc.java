2688b2eb8e02fcbae3907b3f1f6155177be35d1e
Ankit Bhat <ankitbhatpro@gmail.com>
2020-05-11 20:41:18 +0530

    Added functionality to check whether employee is Present or not

--- EmployeeWageCalc.java
+++ EmployeeWageCalc.java
@@ -4,5 +4,11 @@ public class EmployeeWageCalc
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Employee Wage Computation");
+		int FULLTIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if(empCheck == FULLTIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
