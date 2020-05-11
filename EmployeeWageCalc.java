9c8f0a42d8a605979af35bcc5f615e4a8cc781da
Ankit Bhat <ankitbhatpro@gmail.com>
2020-05-11 22:40:13 +0530

    Added part time Employee and wage

--- EmployeeWageCalc.java
+++ EmployeeWageCalc.java
@@ -5,13 +5,22 @@ public class EmployeeWageCalc
 	{
 		System.out.println("Welcome to Employee Wage Computation");
 		int FULLTIME = 1;
+		int PARTTIME = 2;
 		int wagePerHour = 20;
 		int fullTimeHours = 8;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		int partTimeHours = 4;
+		double dailyEmpWage = 0.0;
+		double empCheck = Math.floor(Math.random() * 100) % 3;
 		if(empCheck == FULLTIME)
 		{
-			System.out.println("Employee is Present");
-			double dailyEmpWage =(fullTimeHours*wagePerHour);
+			System.out.println("Employee is Present and Full Time");
+			dailyEmpWage =(fullTimeHours*wagePerHour);
+			System.out.println("Daily Employee Wage is :" + dailyEmpWage);
+		}
+		else if(empCheck == PARTTIME)
+		{
+			System.out.println("Employee is Present and Part Time");
+			dailyEmpWage =(partTimeHours*wagePerHour);
 			System.out.println("Daily Employee Wage is :" + dailyEmpWage);
 		}
 		else
